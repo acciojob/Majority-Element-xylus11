13f7d0384408aee6518e3c46f9ec8fc2f817b377
mangeshahireakaXylus11 <97844580+xylus11@users.noreply.github.com>
2023-10-13 23:50:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-function findMajorityElement(nums) {
-    let candidate = nums[0];
-    let count = 1;
+//your code here
+function findMajorityElement(arr) {
+    var count = 0;
+    var candidate = null;
 
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] === candidate) {
+    // Finding the candidate for majority element
+    for (var i = 0; i < arr.length; i++) {
+        if (count === 0) {
+            candidate = arr[i];
+            count = 1;
+        } else if (arr[i] === candidate) {
             count++;
         } else {
             count--;
         }
+    }
 
-        if (count === 0) {
-            candidate = nums[i];
-            count = 1;
+    // Verifying if the candidate is the majority element
+    count = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === candidate) {
+            count++;
         }
     }
 
-    return candidate;
+    // Checking if the candidate appears more than floor(n/2) times
+    if (count > Math.floor(arr.length / 2)) {
+        return candidate;
+    } else {
+        // Majority element always exists, so this line won't be reached
+        return -1;
+    }
 }
 
-// Test the function
-const nums = [2, 1, 2];
-const majorityElement = findMajorityElement(nums);
+// Example usage
+var inputArray = [2, 1, 2];
+var majorityElement = findMajorityElement(inputArray);
 console.log(majorityElement); // Output: 2
+	
