724910b00b82db24836217d103f361ecf572f158
mangeshahireakaXylus11 <97844580+xylus11@users.noreply.github.com>
2023-10-13 23:44:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+//your code here
+function majorityElement(nums) {
+  let majority = nums[0];
+  let count = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (count === 0) {
+      majority = nums[i];
+      count = 1;
+    } else if (nums[i] === majority) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  return majority;
+}
